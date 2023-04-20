2ef15a40e6ae74aa4945f4dd0316910b29211899
alicjapisk <106108825+alicjapisk@users.noreply.github.com>
2023-04-20 15:47:28 +0200

    added project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'background':"url('../img/background.jpg')",
+        'background2':"url('../img/background2.jpg')",
+        'background3':"url('../img/background3.jpg')",
+      },
+      spacing: {
+        '128': '74rem',
+        '112': '28rem',
+        '114': '43rem',
+      },
+    },
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
